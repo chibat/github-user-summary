4e11e053758dc7b24b18f2b1b8bd8903c38e9b72
Tomofumi Chiba <tomofumi.chiba@gmail.com>
2025-04-25 22:48:00 +0900

    fix

--- mod.ts
+++ mod.ts
@@ -3,14 +3,6 @@
 /**
  * github-user-summary
  *
- * @example
- * ```
- * $ deno -rA jsr:@chiba/github-user-summary chibat
- * stargazers_count: 2916
- * repositories_count: 102
- * all: true
- * ```
- *
  * @module
  */
 
